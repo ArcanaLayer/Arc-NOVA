521b8be46878dc85f0bc927530a99444146a74a9
Arcana Layer <arcanalayer010@gmail.com>
2025-09-09 16:35:49 +0300

    Create main.py

---
+++ sandbox/main.py
@@ -0,0 +1,10 @@
+from kivy.app import app
+from kivy.uix.label import Label
+
+class Nova(app):
+    def build(self):
+        return Label(text = "Welcome Everyone", font_size = '26dp')
+    
+
+if __name__ == "__main__":
+    Nova().run()
